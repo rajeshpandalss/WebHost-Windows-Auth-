68ab42a8d320910de13533bfd2ee6fb46f8943b1
Rajesh <rajesh.panda@lssinc.com>
2017-12-27 18:22:01 +0530

    Add project files.

---
+++ WebHost/Configuration/Clients.cs
@@ -0,0 +1,48 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHost.Configuration
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+            new Client
+                {
+                    ClientName = "JavaScript Implicit Client - Manual",
+                    ClientId = "js.manual",
+                    Flow = Flows.Implicit,
+
+                    AllowedScopes = new List<string>
+                    {
+                        "openid",
+                        "email",
+                        "read",
+                        "write",
+                        "api"
+                    },
+
+                    ClientUri = "https://identityserver.io",
+
+                    RequireConsent = true,
+                    AllowRememberConsent = true,
+
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:37046/index.html",
+                    },
+
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "http://localhost:37046"
+                    }
+                }
+        };
+        }
+    }
+}
---
+++ WebHost/Configuration/Scopes.cs
@@ -0,0 +1,85 @@
+﻿using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHost.Configuration
+{
+    public class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new[]
+                {
+                    ////////////////////////
+                    // identity scopes
+                    ////////////////////////
+
+                    StandardScopes.OpenId,
+                    StandardScopes.Profile,
+                    StandardScopes.Email,
+                    StandardScopes.Address,
+                    StandardScopes.OfflineAccess,
+                    StandardScopes.RolesAlwaysInclude,
+                    StandardScopes.AllClaims,
+
+                    ////////////////////////
+                    // resource scopes
+                    ////////////////////////
+
+                    new Scope
+                    {
+                        Name = "api",
+                        Enabled = true,
+                        DisplayName = "Sample API",
+                        Description = "Access to a simple API",
+                        ScopeSecrets = new List<Secret>
+                        {
+                            new Secret("api-secret".Sha256())
+                        },
+                        Type= ScopeType.Resource
+                    },
+                    new Scope
+                    {
+                        Name = "read",
+                        DisplayName = "Read data",
+                        Type = ScopeType.Resource,
+                        Emphasize = false,
+
+                        ScopeSecrets = new List<Secret>
+                        {
+                            new Secret("secret".Sha256())
+                        }
+                    },
+                    new Scope
+                    {
+                        Name = "write",
+                        DisplayName = "Write data",
+                        Type = ScopeType.Resource,
+                        Emphasize = true,
+
+                        ScopeSecrets = new List<Secret>
+                        {
+                            new Secret("secret".Sha256())
+                        }
+                    },
+                    new Scope
+                    {
+                        Name = "idmgr",
+                        DisplayName = "IdentityManager",
+                        Type = ScopeType.Resource,
+                        Emphasize = true,
+                        ShowInDiscoveryDocument = false,
+
+                        Claims = new List<ScopeClaim>
+                        {
+                            new ScopeClaim(Constants.ClaimTypes.Name),
+                            new ScopeClaim(Constants.ClaimTypes.Role)
+                        }
+                    }
+                };
+        }
+    }
+}
